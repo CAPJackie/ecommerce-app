2cd56a6cd53a05c13019f29b100e8d902594df27
Juan David Ramirez <JuanDavid.Mendoza@cshs.org>
2023-02-16 09:01:55 -0500

    Adding base components to start project

---
+++ components/index.ts
@@ -0,0 +1,6 @@
+import Landing from "./landing/landing";
+import Header from "./header/header";
+import LandingContent from "./landing-content/landing-content";
+import Footer from "./footer/footer";
+
+export { Landing, Header, LandingContent, Footer };
---
+++ components/landing/landing.tsx
@@ -0,0 +1,11 @@
+import { Header, LandingContent, Footer } from "@/components";
+
+export default function Landing() {
+  return (
+    <>
+      <Header />
+      <LandingContent />
+      <Footer />
+    </>
+  );
+}
